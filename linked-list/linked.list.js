64b9de866bae6b31f8a778454423ce7f046e16f8
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2024-10-07 02:55:23 +0300

    findMiddleNode add

--- linked-list/linked.list.js
+++ linked-list/linked.list.js
@@ -164,6 +164,16 @@ class LinkedList {
 		}
 		return this;
 	}
+
+	findMiddleNode() {
+		let slow = this.head;
+		let fast = this.head;
+		while (fast !== null && fast.next !== null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
 }
 
 let myLinkedList = new LinkedList(1);
